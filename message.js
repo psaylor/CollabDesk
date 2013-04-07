9ec6c13de20f5a467506ad48b6cde5865aa157f8
saylorp <psaylor@mit.edu>
2013-04-06 23:14:06 -0400

    Added libraries and index page
    Added both bootstrap and jquery libraries, updated the message class and
    created an index page which loads all the libraries

--- message.js
+++ message.js
@@ -1,17 +1,42 @@
 /**
  * Message represents a message sent from one desk worker to all other desk workers. Messages include information about the author, the date/time of creation, tags and other markers, and the message itself.
+
+Input
+- text
+	a string, the message text
+- author
+	a string, the name of the desk worker
+- tags
+	a list of strings, all tags on either the message or the reply, not sorted
+- type
+	a string, either Note or Issue, use the constants Message.NOTE or Message.ISSUE
+- priority
+	a boolean, use the constants Message.HIGH_PRI or Message.LOW_PRI
+- alert
+	a boolean true or false, whether or not to alert the desk captain, use the constants Message.ALERT or Message.NO_ALERT
+- date (optional)
+	a Date object, encompassing the date and time of the message's creation; if not provided, will use current date and time 
+
  */
  
-var Message = function(text, author, date, time, tags ) {
+
+
+var Message = function(text, author, tags, type, priority, alert, date) {
     ////////////////////////////////////////////////
     // Representation
     //////////
 
 	this.text = text;
 	this.author = author;
-	this.date = date;
-	this.time = time;
 	this.tags = tags;
+	this.alert = alert;
+	this.type = type;
+	this.priority = priority;
+	if (date){
+		this.date = date;
+	} else {
+		this.date = new Date();
+	}
 
 
 	////////////////////////////////////////////////
@@ -20,7 +45,17 @@ var Message = function(text, author, date, time, tags ) {
 
 	this.toString = function(){			
 		
-		var output = this.date + ' ' + this.time + ': ' + this.author + ': ' + this.text + ' -> '+ this.tags ;
+		var output = this.date + ': ' + this.author + ': ' + this.type + ': ' + this.text + '->'+ this.tags +'-> priority: ' + this.priority + '->alert: ' + this.alert;
 		return output;
 	}
 }
+
+////////////////////////////////////////////////
+// Public Constants
+	Message.HIGH_PRI = true;
+	Message.LOW_PRI = false;
+	Message.ALERT = true;
+	Message.NO_ALERT = false;
+	Message.NOTE = "Note";
+	Message.ISSUE = "Issue";
+
