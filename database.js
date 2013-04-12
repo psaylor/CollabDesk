600ade587c2a69e6817e7d239142df965c276441
benjiXie <bxie@MIT.edu>
2013-04-11 23:47:05 -0400

    Basic Search Functionality(view NOTE updated yet)
    database.js: tags now lowercase
    index.html: added search bar and listeners, autocomplete
    message.js: fixed bug where tags were being stored as lists in tag list
    search.js: getMessageIDs(), getMessageTitles()

--- database.js
+++ database.js
@@ -31,7 +31,7 @@ var Database = function() {
 		for (var i = 0; i < msg.tags.length; i++) {
 			var tag = msg.tags[i];
 			if( this.tags.indexOf(tag) == -1) {
-				this.tags.push(tag);
+				this.tags.push(tag[0].toLowerCase()); //tags MUST be lowercase (for search)
 			}
 		};
 	}
