6bfc612b6dc88566c1bb50f91b61270e6065b92a
saylorp <psaylor@mit.edu>
2013-04-16 19:27:50 -0400

    added browsepane.js from other branch

---
+++ BrowsePane.js
@@ -0,0 +1,47 @@
+/** BROWSE PANE DOCUMENTATION
+ * Handles functionality for Browse Pane, which displays all the messages
+ */
+
+var BrowsePane = function(){
+	/**
+	* Converts a message to HTML to be displayed in the BrowserPane
+	*
+	*/
+	this.getMessageHTML=function(msg){
+		var title = msg.title;
+		var text = msg.text.substring(0,125);
+		if(msg.text.length>125){ //if message was truncated to display
+			text+="...";
+		}
+		console.log("title: "+title);
+		console.log("text:" +text);
+
+		var output ="<tr><td><div class='message'><div class='title'>"
+					+title+"</div><div class='text'>"+text+"</div></div></td></tr>";
+		console.log(output);
+		return output;
+	};
+
+	/**
+	* Post all messages stored in database onto browser pane
+	*
+	*/
+	this.getAllMessageHTML=function(database)	{
+		var msgList = database.getAllMessages(); //list of message objects
+		var output="";
+		for (var i=msgList.length-1; i>=0; i--){
+			output+=this.getMessageHTML(msgList[i]);
+		}
+		return output
+
+	};
+
+	/**
+	* Moves message from unread list to list with dates
+	*
+	*/
+	this.moveReadMessage=function(message){
+
+	};
+
+};
