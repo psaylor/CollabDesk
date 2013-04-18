6fddcbc6110b519c7bdc0480da974af6451be5f5
Jonathan Lui <jlui@mit.edu>
2013-04-17 21:34:13 -0400

    Radio Button Functionality Added

--- createMessage.js
+++ createMessage.js
@@ -4,11 +4,16 @@ function reset() {
 	$("#prependedtext").val("");
 	// reset the template to the default value
 
-	var formTitleBar = "New message <div class='floatRight btn'><i class='icon-chevron-down' id='chevronDown'></i></div>";
+	var formTitleBar = "Create New Message<div class='floatRight btn'><i class='icon-chevron-down' id='chevronDown'></i></div>";
 	$("#formTitle").html(formTitleBar);
 	$("#submitBtn").html("Submit Message");
 
 	$("select#selectTemplate").val("None Selected");
+
+	// Uncheck the radio buttons
+	$(".btn-group .btn.active").each(function() {
+		$("#" + this.id).removeClass("active");
+	});
 }
 
 function submit() {
@@ -16,17 +21,35 @@ function submit() {
 	var text = $("#textarea").val();
 	var tags = $("#prependedtext").val();
 
-	// need to grab three side button data
-
 	tags = tags.split(",");
 	for (var i = 0; i < tags.length; i++) {
 		tags[i] = tags[i].replace(/\s/g, '');
 	}
 
 	var author = 'Kayla';
-	var type = Message.NOTE;
-	var priority = Message.HIGH_PRI;
-	var alert = false;
+
+	// Determine which buttons are pressed
+	var noteIssue = "ni-btn";
+	var hiLowPriority = "pri-btn";
+	var alertCpt = "alert-btn";
+	var activeBtns = {noteIssue : Message.NOTE, hiLowPriority : Message.LOW_PRI, alertCpt : Message.NO_ALERT } ;
+	$(".btn-group .btn.active").each(function() {
+		switch(this.id) {
+			case noteIssue:
+				activeBtns[noteIssue] = Message.ISSUE;
+				break;
+			case hiLowPriority:
+				activeBtns[hiLowPriority] = Message.HIGH_PRI;
+				break;
+			case alertCpt:
+				activeBtns[alertCpt] = Message.ALERT;
+				break;
+		}
+	});
+
+	var type = activeBtns[noteIssue];
+	var priority = activeBtns[priority];
+	var alert = activeBtns[alert];
 	var date = new Date('17 Mar, 2013 15:15:00');
 
 	var msg = new Message(title, text, author, tags, type, priority, alert, date);
--- createReply.js
+++ createReply.js
@@ -1,4 +1,27 @@
+function reset() {
+	$("#textinput").val("");
+	$("#textarea").val("");
+	$("#prependedtext").val("");
+	// reset the template to the default value
+
+	var formTitleBar = "Create New Message<div class='floatRight btn'><i class='icon-chevron-down' id='chevronDown'></i></div>";
+	$("#formTitle").html(formTitleBar);
+	$("#submitBtn").html("Submit Message");
+
+	$("select#selectTemplate").val("None Selected");
+
+	// Uncheck the radio buttons
+	$(".btn-group .btn.active").each(function() {
+		$("#" + this.id).removeClass("active");
+	});
+}
+
 function openReplyForm() {
+	// Uncheck the radio buttons
+	$(".btn-group .btn.active").each(function() {
+		$("#" + this.id).removeClass("active");
+	});
+
 	$("select#selectTemplate").val("None Selected");
 
 	var formTitleBar = "Reply <div class='floatRight btn'><i class='icon-chevron-down' id='chevronDown'></i></div>";
@@ -49,10 +72,5 @@ function submitReply() {
 	var formTitleBar = "New message <div class='floatRight btn'><i class='icon-chevron-down' id='chevronDown'></i></div>";
 	$("#formTitle").html(formTitleBar);
 
-	$("#textinput").val("");
-	$("#textarea").val("");
-	$("#prependedtext").val("");
-	// reset the template to the default value
-
-	$("select#selectTemplate").val("None Selected");
+	reset();
 }
