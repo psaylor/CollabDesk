0a49a6e28c99691e741db9e6d906ad1948e7691e
saylorp <psaylor@mit.edu>
2013-05-08 01:17:49 -0400

    idk what i've done

--- backend.js
+++ backend.js
@@ -430,44 +430,45 @@ function markRead(msg) {
 
 
 
-var title = 'Visa card found';
-	var text = 'A visa card was found on one of the tables at dining today. It does not have a name on it.';
-	var author = 'Kayla';
-	var tags = ['lost/found'];
-	var type = NOTE;
-	var priority = HIGH_PRI;
-	var alert = false;
-	var date = new Date('17 Mar, 2013 15:15:00');
+// var title = 'Visa card found';
+// 	var text = 'A visa card was found on one of the tables at dining today. It does not have a name on it.';
+// 	var author = 'Kayla';
+// 	var tags = ['lost/found'];
+// 	var type = NOTE;
+// 	var priority = HIGH_PRI;
+// 	var alert = false;
+// 	var date = new Date('17 Mar, 2013 15:15:00');
 
-	title = 'Package for Jane';
-	text = "A package came in for Jane, but she doesn't live here, and I can't find any record of her. It's in bin 6 for now.";
-	author = "Ben";
-	tags = ['package'];
-	type = Message.ISSUE;
-	priority = Message.LOW_PRI;
-	alert = true;
-	var date = new Date('17 Mar, 2013 10:28:00');
+// 	title = 'Package for Jane';
+// 	text = "A package came in for Jane, but she doesn't live here, and I can't find any record of her. It's in bin 6 for now.";
+// 	author = "Ben";
+// 	tags = ['package'];
+// 	type = Message.ISSUE;
+// 	priority = Message.LOW_PRI;
+// 	alert = true;
+// 	var date = new Date('17 Mar, 2013 10:28:00');
 
 // var msg = Message.create(title, text, author, tags, type, priority, alert, date);
 // console.log("done with message.");
 // console.log(msg);
 // console.log('message day: ' + msg.getDay());
 
-// title = 'Fixed';
-// 	text = 'Sally picked up her id. All better =]';
-// 	author = 'Sam';
-// 	tags = ['fixed'];
-// 	date = new Date('19 Mar, 2013 15:31:00');
-// 	var reply = Reply.create(title, text, author, tags, date, msg);
-// 	// msg.addReply(reply);
-// 	console.debug("msg day: " + msg.getDay());
+
+	// msg.addReply(reply);
+	// console.debug("msg day: " + msg.getDay());
 
 // var MsgQuery = new Parse.Query(Message);
 // var msg2 = null;
-// MsgQuery.get("6LU7Ua5OEp", {
+// MsgQuery.get("POqBGuEw1S", {
 // 	success: function(object) {
 // 		msg2 = object;
 // 		console.debug('successful get msg');
+// 		title = 'Returned';
+// 	text = 'Mary picked up her ID =]';
+// 	author = 'Sam';
+// 	tags = ['returned'];
+// 	date = new Date('19 Mar, 2013 15:31:00');
+// 	var reply = Reply.create(title, text, author, tags, date, msg2);
 // 	},
 // 	error : function(object, error) {
 // 		console.debug('error getting object');
