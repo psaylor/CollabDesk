9d5b09ccff890cf0b95ef53a4587f53967545756
benjiXie <bxie@MIT.edu>
2013-05-14 15:15:30 -0400

    Search fixed (empty search bar cancels search, 'esc' cancels search)

--- BrowsePaneListeners.js
+++ BrowsePaneListeners.js
@@ -1,44 +1,57 @@
 var spinner = '<i class="icon-spinner icon-spin icon-3x"></i>';
 
 function displayUnreadMessages(unreadList) {
-var unreadOutput="";
-        unreadOutput+="<div class='group'><ul id='unread-content'>";
-        if(unreadList.length>0){
-            for (var i=0; i< unreadList.length; i++){
+    var unreadOutput="";
+    unreadOutput+="<div class='group'><ul id='unread-content'>";
+    
+    // var group = $(document.createElement('div')).addClass('group');
 
-                msg=unreadList.at(i);
-                var title = msg.get('title');
-                var date = msg.get('date');
-                var dateStr = date.getMonth()+1+"/"+date.getDate()+"/"+date.getFullYear();
-                var text = msg.get('text').substring(0,125);
-                var id = msg.id;
-                var alert=msg.get('alert'); //boolean
-                var priority = msg.get('priority'); //boolean
-                var noteVsIssue = msg.get('type'); //string
-                if(msg.get('text').length>125){ //if message was truncated to display
-                    text+="...";
-                }
+    // //messages (<li>) in here
+    // var ul = $(document.createElement('ul'))
+    //     .attr('id', 'unread-content');
+
+    // $(bucket).append(outputDate);
+    // //$(group).append(bucket);
+    // $(group).append(ul);
+
+    if(unreadList.length>0){
+        for (var i=0; i< unreadList.length; i++){
+
+            //$(ul).append(unreadList.at(i));
 
 
-
-                unreadOutput +="<li class = 'message' id='"+id+"'>"+
-                            "<div class='message-metadata'><div class='timestamp'>"+dateStr+"</div>"+
-                            //"<i class='icon-exclamation-sign icon-color'></i><i class='icon-reorder icon-color'></i><i class='icon-envelope icon-color'></i></div>"+
-                            determineIcons(alert, priority, noteVsIssue)+ "</div>"+
-                            "<div class='message-content'>"+
-                            "<div class='title'>"+title+"</div><div class='text'>"+text+"</div>"+
-                            "</div>"+
-                            "</li>";
+            var title = msg.get('title');
+            var date = msg.get('date');
+            var dateStr = date.getMonth()+1+"/"+date.getDate()+"/"+date.getFullYear();
+            var text = msg.get('text').substring(0,125);
+            var id = msg.id;
+            var alert=msg.get('alert'); //boolean
+            var priority = msg.get('priority'); //boolean
+            var noteVsIssue = msg.get('type'); //string
+            if(msg.get('text').length>125){ //if message was truncated to display
+                text+="...";
             }
-            unreadOutput+="</ul>";
-        } else {
-            unreadOutput = getNoUnreadAlert();
+
+
+
+            unreadOutput +="<li class = 'message' id='"+id+"'>"+
+                        "<div class='message-metadata'><div class='timestamp'>"+dateStr+"</div>"+
+                        //"<i class='icon-exclamation-sign icon-color'></i><i class='icon-reorder icon-color'></i><i class='icon-envelope icon-color'></i></div>"+
+                        determineIcons(alert, priority, noteVsIssue)+ "</div>"+
+                        "<div class='message-content'>"+
+                        "<div class='title'>"+title+"</div><div class='text'>"+text+"</div>"+
+                        "</div>"+
+                        "</li>";
         }
-        
-        console.log('output string for unread msgs');
-        // console.log(unreadOutput);
-        $("#unread-table").html(unreadOutput);
-        addClickListener();
+        unreadOutput+="</ul>";
+    } else {
+        unreadOutput = getNoUnreadAlert();
+    }
+    
+    console.log('output string for unread msgs');
+    // console.log(unreadOutput);
+    $("#unread-table").html(unreadOutput);
+    addClickListener();
 }
 
 function onClickUnreadTab() {
@@ -63,34 +76,40 @@ function browsePaneReady() {
         //console.log(inp);
         //var messageIDs=getMessageIDs(inp);
         if(inp.length>0){
-            updateSearchedBrowsePane("message-table", inp);
+            updateSearchedBrowsePane(inp);
             addClickListener();
         }
         //$(".ui-menu-item").hide()
     });
 
     $("#search-tbox").keydown(function(event){
+        console.log("keydown listener search------------------------------------------");
         var inp=$("#search-tbox").val();
-        
-        // if(!popoverShowing){
-        // 	$(this).popover('show');
-        // 	popoverShowing=true;
-        // }
 
-        if(event.which==13 && inp.length>0){ //checking for 'enter'
+        if(event.which==13){
+            if(inp.length>0){ //checking for 'enter'
             //console.log("Listening to ENTER. input is "+inp+"matching messages are ");
             //console.log("messageIDs");
-            updateSearchedBrowsePane("message-table", inp);
+            updateSearchedBrowsePane(inp);
             addClickListener();
             //$(".ui-menu-item").hide()
+            }
+            else{
+                updateBrowsePane();
+            }
         }
-
-        if(inp==null || inp.length<2){
-            console.log("ELSE "+inp);
-            updateBrowsePane("message-table");
+        //escape key clears search
+        else if(event.which==27){
+            $("#search-tbox").val("");
+            updateBrowsePane();
+        }
+        else if(inp==null || inp.length<2){
+            console.log('NULL OR <2 INPUT: BROWSE PANE RETURNING');
+            updateBrowsePane();
         }
     });
 
+    //not currently functioning
     $("#search-tbox").autocomplete({
         source: function(request, response){
             //console.log(request.term);
@@ -100,11 +119,11 @@ function browsePaneReady() {
                 });
                 var inp=$("#search-tbox").val();
                 //var messageIDs=getMessageIDs(inp);
-                updateSearchedBrowsePane("message-table", inp);
+                updateSearchedBrowsePane(inp);
                 addClickListener();
             }
             else{
-            	updateBrowsePane("message-table");
+            	updateBrowsePane();
             }
         }
     });
@@ -112,30 +131,10 @@ function browsePaneReady() {
     $('#unread-tab').click(onClickUnreadTab);
     $('#all-tab').click(onClickAllTab);
 
-    // updateUnreadCount();
-
-    // //Advanced Search Popover
-    // $('#search-tbox').blur(function(event){
-    // 	console.log(event);
-    // 	console.log("FOCUS IS BELOW");
-    // 	console.log($(':focus'));
-    // 	if(!$('.popover').is('focus')){
-    // 		//$(this).popover('hide');
-    // 		//popoverShowing=false;
-    // 	}
-    // });
-
-    // $('#search-tbox').popover({
-    // 	title: 'Search Options',
-    // 	trigger: 'manual',
-    // 	html: true,
-    // 	content: showSearchDetails()
-    // });
-
 }
 
 function addClickListener(){
-    console.log("in addClickListener()");
+    //console.log("in addClickListener()");
     $(".message").click(function(){
         console.log("CLICKED ON "+$(this).attr('id'));
         var focusedId=$(this).attr("id"); //id of message that is clicked
@@ -150,7 +149,7 @@ function addClickListener(){
 }
 
 function addUnreadListeners() {
-    console.log('adding listeners to unread messages');
+    //console.log('adding listeners to unread messages');
     updateUnreadCount();
     $('#unread-table .message').click(function() {
         //marking message as read
